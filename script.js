9695e5a0633da779860efcf3e47cb847a78cbadc
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-07-07 22:40:36 +0000

    minor update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     option=()=>{
     //array with the words
-    var first = 'A ';
+    var first = ['A ', 'The ' ];
     var adj = ['two headed ', 'nuclear ', 'angry ', 'lonely ', 'crazy ', 'glowing ', 'smelly ', 'slow ', 'old '];
     var noun = ['jogger ', 'racoon ', 'dog ', 'merchant ', 'driver ', 'comedian ', 'pinecone '];
     var action = ['took my ', 'threw my ', 'yelled at my ', 'kicked my ', 'stole my ', 'burned my ', 'bit my ', 'hit my '];
@@ -9,6 +9,7 @@ window.onload = () => {
     var where = ['on the street', 'in my house', 'in my driveway', 'in front of the office', 'near the mall', 'near the toilet', 'at the bus station'];
 
     // declaring random variables
+    var rdm0 = Math.floor((Math.random() * first.length));
     var rdm1 = Math.floor((Math.random() * adj.length));
     var rdm2 = Math.floor((Math.random() * noun.length));
     var rdm3 = Math.floor((Math.random() * action.length));
@@ -16,6 +17,6 @@ window.onload = () => {
     var rdm5 = Math.floor((Math.random() * where.length));
 
     //creating a sentence (the excuse)
-    document.querySelector('#theexcuse').innerHTML = first + adj[rdm1] + noun[rdm2] + action[rdm3] + possetion[rdm4] + where[rdm5];
+    document.querySelector('#theexcuse').innerHTML = first[rdm0] + adj[rdm1] + noun[rdm2] + action[rdm3] + possetion[rdm4] + where[rdm5];
     };
 };
